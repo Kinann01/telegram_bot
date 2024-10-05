efb70ce88c0c1dfe5d996ffe352558c2d0df4730
Kinan <kinanalfalakh@gmail.com>
2024-10-06 00:49:29 +0200

    added source file - main.py && requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# telegram_bot
-Simple telegram anti casino ADs bot - deletes messages based on keywords if given admin rights - based on TG libraries and frameworks
+# Telegram Bot runs on Render.com
---
+++ main.py
@@ -0,0 +1,36 @@
+from telegram.ext import Updater, MessageHandler, Filters
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler('bot.log', maxBytes=5*1024*1024, backupCount=3)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+KEYWORDS = ['casino', 'jackpot', '2000$', '1000$', '500$', '300$', '200$', '100$', 'Казино', 'ставка', 'слоты', 'джекпот', 'kазино']
+
+def delete_casino_messages(update, context):
+    if update.message is not None:
+        message_text = update.message.text or update.message.caption
+        if message_text:
+            message_text = message_text.lower()
+            if any(keyword in message_text for keyword in KEYWORDS):
+                try:
+                    update.message.delete()
+                    logger.info(f"Deleted a message from {update.message.from_user.username} due to containing prohibited keywords.")
+                except Exception as e:
+                    logger.error(f"Error deleting message: {str(e)}")
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.text | Filters.caption & Filters.chat_type.groups, delete_casino_messages))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
