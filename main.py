382a82389ed32889611beef6da92a24268949286
Kinan <kinanalfalakh@gmail.com>
2024-10-06 15:09:57 +0200

    added local polling mode

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telegram.ext import Updater, MessageHandler, Filters
 import os
 
+
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 KEYWORDS = ['casino', 'jackpot', '2000$', '1000$', '500$', '300$', '200$', '100$', 'Казино', 'ставка', 'слоты', 'джекпот', 'kазино']
 
@@ -22,8 +23,12 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(Filters.text | Filters.caption & Filters.chat_type.groups, delete_casino_messages))
 
-    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-    updater.bot.setWebhook(f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME')}/{TOKEN}")
+    if os.getenv("RENDER_EXTERNAL_HOSTNAME"):
+        updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+        updater.bot.setWebhook(f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}/{TOKEN}")
+    else:
+        updater.start_polling()
+
     updater.idle()
 
 if __name__ == '__main__':
